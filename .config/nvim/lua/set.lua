d7cb264fb09bdd53f731758ba2a5d1c108f4d084
gaesa <71256557+gaesa@users.noreply.github.com>
2023-02-13 01:46:17 +0800

    Better default options
    1. Hide intro message
    2. Show filename as window title
    3. Hide tilde "~"

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -1,3 +1,12 @@
+-- Hide intro message
+vim.opt.shortmess:append("I")
+
+-- Show filename as window title
+vim.opt.title = true
+
+-- Hide tilde "~" at the start of nonexistent line
+vim.opt.fillchars:append("eob: ")
+
 -- Encoding
 vim.opt.fileencodings = "ucs-bom,utf-8,gbk,sjis,euc-jp,big5,gb18030,latin1"
 -- EOL
@@ -12,7 +21,7 @@ vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 50
 
 -- System clipboard
-vim.opt.clipboard = "unnamedplus"
+-- vim.opt.clipboard = "unnamedplus"
 
 -- TAB/Indent
 vim.opt.tabstop = 4
