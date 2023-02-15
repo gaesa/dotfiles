0fedd4bde3e57b0b3f30b511078380a1832af38f
gaesa <71256557+gaesa@users.noreply.github.com>
2023-02-15 20:49:46 +0800

    Highlight yanked text

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -10,7 +10,7 @@ autocmd(
     "BufReadPost",
     { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
-autocmd("FileType", { pattern = { "gitcommit", }, command = [[normal! gg]] })
+autocmd("FileType", { pattern = { "gitcommit" }, command = [[normal! gg]] })
 autocmd("BufWinEnter", { command = [[normal! zz]] })
 
 -- Remove all trailing whitespace
@@ -18,3 +18,11 @@ autocmd("BufWritePre", { pattern = { "*" }, command = [[silent! %s/\s\+$//e]] })
 
 -- Retab
 autocmd("BufWritePre", { pattern = { "*" }, command = [[silent! %retab]] })
+
+-- Highlight yanked text
+autocmd("TextYankPost", {
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 80 })
+    end,
+})
