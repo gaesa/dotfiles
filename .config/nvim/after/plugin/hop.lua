4ade35971b9a866559e5ab023d199ed515020840
gaesa <71256557+gaesa@users.noreply.github.com>
2023-02-16 14:02:35 +0800

    Change some functions of hop.nvim shortcuts

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -4,23 +4,33 @@ local directions = require("hop.hint").HintDirection
 hop.setup({})
 
 vim.keymap.set("", "f", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
 end, { remap = true })
+
 vim.keymap.set("", "F", function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
 end, { remap = true })
+
 vim.keymap.set("", "t", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
 end, { remap = true })
+
 vim.keymap.set("", "T", function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
 end, { remap = true })
+
+vim.keymap.set("", "gl", function()
+    hop.hint_vertical({ multi_windows = true })
+end, { remap = true })
+
 vim.keymap.set("", "s", function()
     hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
 end, { remap = true })
+
 vim.keymap.set("", "S", function()
     hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
 end, { remap = true })
+
 vim.keymap.set("", "gs", function()
-    hop.hint_words({ multi_windows = true, current_line_only = false })
+    hop.hint_words({ multi_windows = true })
 end, { remap = true })
