3173ff1a8a79382f5ba70f62010d83918e83a953
gaesa <71256557+gaesa@users.noreply.github.com>
2023-03-03 10:18:10 +0800

    Minor change about hop.nvim

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -4,19 +4,19 @@ local directions = require("hop.hint").HintDirection
 hop.setup({})
 
 vim.keymap.set("", "f", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, { remap = true })
 
 vim.keymap.set("", "F", function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 end, { remap = true })
 
 vim.keymap.set("", "t", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
 end, { remap = true })
 
 vim.keymap.set("", "T", function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, { remap = true })
 
 vim.keymap.set("", "<leader>l", function()
