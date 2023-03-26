89e9ffcc6e798ae8f1d765d164a0f5076987fb80
gaesa <71256557+gaesa@users.noreply.github.com>
2023-03-26 19:36:46 +0800

    Use `~` path to describe path of neovim configurations
    Reason: keep a consistent experience when opening configurations in different
    workdir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,13 +3,13 @@ require("let")
 require("remap")
 require("autocmd")
 require("plugins")
--- ./.config/nvim/plugin/colors.lua
--- ./.config/nvim/plugin/lualine.lua
--- ./.config/nvim/plugin/indent-blankline.lua
--- ./.config/nvim/plugin/telescope.lua
--- ./.config/nvim/plugin/treesitter.lua
--- ./.config/nvim/plugin/lsp.lua
--- ./.config/nvim/plugin/luasnip.lua
--- ./.config/nvim/plugin/undotree.lua
--- ./.config/nvim/plugin/harpoon.lua
--- ./.config/nvim/plugin/hop.lua
+-- ~/.config/nvim/plugin/colors.lua
+-- ~/.config/nvim/plugin/lualine.lua
+-- ~/.config/nvim/plugin/indent-blankline.lua
+-- ~/.config/nvim/plugin/telescope.lua
+-- ~/.config/nvim/plugin/treesitter.lua
+-- ~/.config/nvim/plugin/lsp.lua
+-- ~/.config/nvim/plugin/luasnip.lua
+-- ~/.config/nvim/plugin/undotree.lua
+-- ~/.config/nvim/plugin/harpoon.lua
+-- ~/.config/nvim/plugin/hop.lua
