5d9358a42c9c84e598aea13b113affde9640c0cc
gaesa <71256557+gaesa@users.noreply.github.com>
2023-05-23 21:12:27 +0800

    feat(theme): auto switch theme for nvim and more
    More consistent and clearer code

--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -1,61 +1,78 @@
-function GreatTheme(color, time)
-    local colorpath = vim.fn.stdpath("data") .. "/lazy/" .. color
-    if vim.loop.fs_stat(colorpath) then
-        color = color
+local function load_theme(color)
+    vim.opt.background = color
+
+    local function set_theme(theme)
         vim.opt.termguicolors = true
-        -- vim.cmd([[let g:everforest_better_performance = 1]])
-        vim.g.everforest_better_performance = 1
+        vim.cmd([[let g:everforest_better_performance = 1]])
         -- vim.g.everforest_background = 'soft'
-        if time == "night" then
+
+        if color == "dark" then
             -- Blow two lines code produce different effects
-            -- Maybe related features are not 100% exposed to Lua yet
             -- vim.g.erverforest_transparent_background = 1
             vim.cmd([[let g:everforest_transparent_background = 1]])
-        else
-            vim.g.erverforest_transparent_background = 0
-        end
-    else
-        if time == "night" then
-            color = "habamax"
-            vim.opt.cursorline = true
-        else
-            color = "shine"
-        end
-    end
-    vim.cmd.colorscheme(color)
-    if color == "everforest" then
-        if time == "day" then
-            vim.opt.cursorline = true
-        else
+            vim.cmd.colorscheme(theme)
             vim.opt.cursorlineopt = "number"
             vim.opt.cursorline = true
+        else
+            vim.cmd([[let g:everforest_transparent_background = 0]])
+            vim.cmd.colorscheme(theme)
+            vim.opt.cursorline = true
         end
-        if vim.wo.diff == true then
+
+        if vim.wo.diff then
             vim.opt.cursorline = false
         else
             return
         end
+    end
+
+    local function set_fallback_theme()
+        local theme
+        if color == "dark" then
+            vim.opt.cursorline = true
+            theme = "habamax"
+        else
+            theme = "shine"
+        end
+        vim.cmd.colorscheme(theme)
+    end
+
+    local theme = "everforest"
+    local colorpath = vim.fn.stdpath("data") .. "/lazy/" .. theme
+    if vim.loop.fs_stat(colorpath) then
+        set_theme(theme)
     else
-        return
+        set_fallback_theme()
     end
 end
 
-if vim.env.TERM ~= "linux" then
-    local color = "everforest"
-    -- if vim.fn.strftime("%H:%M")
-    if os.date("%H:%M") > "06:00" and os.date("%H:%M") < "18:00" then
-        local time = "day"
-        vim.opt.background = "light"
-        GreatTheme(color, time)
+local function auto_switch_theme()
+    local hour = tonumber(vim.fn.strftime("%H"))
+    local color
+    if hour >= 6 and hour < 18 then
+        color = "light"
     else
-        local time = "night"
-        vim.opt.background = "dark"
-        GreatTheme(color, time)
+        color = "dark"
     end
-else
-    if vim.wo.diff == true then
-        vim.cmd([[colorscheme default]])
+    load_theme(color)
+end
+
+local function main()
+    if vim.env.TERM ~= "linux" then
+        ---@diagnostic disable-next-line: undefined-field
+        local color = vim.g.mycolor
+        if color then
+            load_theme(color)
+        else
+            auto_switch_theme()
+        end
     else
-        vim.cmd([[colorscheme habamax]])
+        if vim.wo.diff then
+            vim.cmd.colorscheme("default")
+        else
+            vim.cmd.colorscheme("habamax")
+        end
     end
 end
+
+main()
