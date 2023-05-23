34bc65a809fe217865b277ed4235eeaef82c53de
gaesa <71256557+gaesa@users.noreply.github.com>
2023-05-23 20:05:05 +0800

    feat(indent-blankline): disable for some filetypes

--- .config/nvim/plugin/indent-blankline.lua
+++ .config/nvim/plugin/indent-blankline.lua
@@ -3,3 +3,19 @@ require("indent_blankline").setup({
     show_first_indent_level = false,
     show_trailing_blankline_indent = false,
 })
+
+-- Disable indent-blankline for some filetypes
+local excluded_files = { "scheme", "lisp" }
+
+local autocmd = vim.api.nvim_create_autocmd
+local group = vim.api.nvim_create_augroup("indent-blankline.conf", { clear = true })
+
+autocmd({ "FileType" }, {
+    pattern = excluded_files,
+    callback = function()
+        require("indent_blankline").setup({
+            enabled = false,
+        })
+    end,
+    group = group,
+})
