e6fc37960d6a0491b0704e116634d0aefdb625f4
gaesa <71256557+gaesa@users.noreply.github.com>
2023-05-23 17:28:16 +0800

    fix(luasnip): restrict filetypes

--- .config/nvim/plugin/luasnip.lua
+++ .config/nvim/plugin/luasnip.lua
@@ -11,13 +11,22 @@ ls.add_snippets("all", {
     s("hi", {
         t("Hello, world!"),
     }),
-    s({ name = "Shebang for C", trig = "sbc", dscr = "Put Clang shebang for C" }, {
-        t([[//usr/bin/clang "$0" -o "${o=$(mktemp)}" && "${o}" "$@" && rm "${o}" && exit]]),
-    }),
+})
+
+ls.add_snippets("python", {
     s({ name = "Shebang for Python", trig = "sbpy", dscr = "Put shebang for Python" }, {
         t([[#!/usr/bin/env python3]]),
     }),
+})
+
+ls.add_snippets("sh", {
     s({ name = "Better bash", trig = "set", dscr = "Fail fast and be aware of exit codes" }, {
         t([[set -Eeuo pipefail]]),
+    }),
+})
+
+ls.add_snippets("c", {
+    s({ name = "Shebang for C", trig = "sbc", dscr = "Put Clang shebang for C" }, {
+        t([[//usr/bin/clang "$0" -o "${o=$(mktemp)}" && "${o}" "$@" && rm "${o}" && exit]]),
     }),
 })
