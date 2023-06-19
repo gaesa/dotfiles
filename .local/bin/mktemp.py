ba95977f5979b605ff3118badaf17724195abb40
gaesa <71256557+gaesa@users.noreply.github.com>
2023-06-19 12:07:26 +0800

    refactor(mktemp): more modular

---
+++ .local/bin/mktemp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from uuid import uuid4
+from os import getenv, chmod, stat
+from stat import S_IMODE
+from os.path import join
+
+
+def mktemp(prefix=""):
+    if prefix != "":
+        name = f"{prefix}-{str(uuid4())}"
+    else:
+        name = str(uuid4())
+    dir = getenv("XDG_RUNTIME_DIR", "/tmp")
+    path = join(dir, name)
+    open(path, "w").close()
+
+    permission = oct(S_IMODE(stat(path).st_mode))
+    if permission != 0o600:
+        chmod(path, 0o600)
+        return path
+    else:
+        return path
