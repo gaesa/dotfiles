36aa9ef433532b653d647287037d715198bacb25
gaesa <71256557+gaesa@users.noreply.github.com>
2023-06-27 19:21:13 +0800

    feat(lf): preview within sandbox using the bwrap

---
+++ .config/lf/previewSandbox.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+( # `--new-session` doesn't work
+    exec bwrap \
+        --ro-bind /usr/bin /usr/bin \
+        --ro-bind /usr/share/ /usr/share/ \
+        --ro-bind /usr/lib /usr/lib \
+        --ro-bind /usr/lib64 /usr/lib64 \
+        --symlink /usr/bin /bin \
+        --symlink /usr/lib64 /lib64 \
+        --proc /proc \
+        --ro-bind ~/.config ~/.config \
+        --ro-bind "$PWD" "$PWD" \
+        --dev-bind /dev/tty /dev/tty \
+        --dev-bind /dev/null /dev/null \
+        --dev-bind /tmp /tmp \
+        --bind ~/.cache/lf_thumb ~/.cache/lf_thumb \
+        --bind ~/.config/lf/__pycache__ ~/.config/lf/__pycache__ \
+        --unshare-all \
+        ~/.config/lf/preview.py "$@"
+)
