d7e4778dffeea366ea45148fd8a76f574f45c03f
gaesa <71256557+gaesa@users.noreply.github.com>
2023-09-08 03:45:18 +0800

    chore(nvim): update keymaps of `telescope`

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -38,10 +38,10 @@ return {
 
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>pf", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>bf", builtin.buffers, {})
+            vim.keymap.set("n", "<C-h>", builtin.help_tags, {})
 
             -- To get fzf loaded and working with telescope, you need to call
             -- load_extension, somewhere after setup function:
