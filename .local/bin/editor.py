35b40b2061f2082a971c5f122785bbc3f3289693
gaesa <71256557+gaesa@users.noreply.github.com>
2023-09-12 20:16:36 +0800

    chore(editor): add type hints & prefer full path

--- .local/bin/editor.py
+++ .local/bin/editor.py
@@ -3,11 +3,11 @@ from sys import argv
 from subprocess import run
 
 
-def edit(files=argv[1:]):
+def edit(files: list[str] | tuple[str, ...] = argv[1:]):
     if files == []:
-        run(["nvim"])
+        run(["/usr/bin/nvim"])
     else:
-        run(["nvim", "-o", *files, "-c", "wincmd H"])
+        run(["/usr/bin/nvim", "-o", *files, "-c", "wincmd H"])
 
 
 if __name__ == "__main__":
