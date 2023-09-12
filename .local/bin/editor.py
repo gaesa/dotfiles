b5fe04f32be46b8cdb91f148128c8c27c2b0812c
gaesa <71256557+gaesa@users.noreply.github.com>
2023-09-12 20:16:36 +0800

    fix(emacs): make emacsclient work in lf

--- .local/bin/editor.py
+++ .local/bin/editor.py
@@ -13,22 +13,29 @@ def open_with_nvim(files):
         return
 
 
-def open_with_emacs(files):
-    if files == []:
+def open_with_emacs(files: list[str], allow_empty: bool = False):
+    if files == [] and (not allow_empty):
         return
     else:
         len_files = len(files)
-        sexp = (
-            "(progn (call-interactively #'+workspace/delete)"
-            f'      (find-file "{files[0]}") '
-        )
-        if len_files == 1:
-            sexp += ")"
+        if files == [] and allow_empty:
+            sexp = "(call-interactively #'+workspace/delete)"
         else:
-            sexp += f'(split-window-right) (other-window 1) (find-file "{files[1]}") '
-            for file in files[2:]:
-                sexp += f'(split-window-below) (other-window 1) (find-file "{file}") '
-            sexp += "(other-window 1))"
+            sexp = (
+                "(progn (call-interactively #'+workspace/delete)"
+                f'      (find-file "{files[0]}") '
+            )
+            if len_files == 1:
+                sexp += ")"
+            else:
+                sexp += (
+                    f'(split-window-right) (other-window 1) (find-file "{files[1]}") '
+                )
+                for file in files[2:]:
+                    sexp += (
+                        f'(split-window-below) (other-window 1) (find-file "{file}") '
+                    )
+                sexp += "(other-window 1))"
         p = Popen(
             [
                 "/usr/bin/emacsclient",
@@ -49,7 +56,7 @@ def wait_editor(*editors: Popen[bytes] | None):
     )
 
 
-def open_with(nvim_files, emacs_files):
+def open_with(nvim_files: list[str], emacs_files: list[str]):
     nvim = open_with_nvim(nvim_files)
     emacs = open_with_emacs(emacs_files)
     return nvim, emacs
@@ -59,7 +66,7 @@ def split_cond(file: str) -> bool:
     return splitext(file)[1] in {".org", ".el", ".scm", ".ss", ".rkt"}
 
 
-def edit(files=argv[1:]):
+def edit(files: list[str] | tuple[str, ...] = argv[1:]):
     if files == []:
         run(["/usr/bin/nvim"])
     else:
---
+++ .local/bin/emacs.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from sys import argv
+from editor import open_with_emacs
+
+open_with_emacs(argv[1:], allow_empty=True)
