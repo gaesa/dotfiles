6372da601a24bdbe3fd97304727fad5dad754b70
gaesa <71256557+gaesa@users.noreply.github.com>
2023-09-13 02:23:34 +0800

    refactor(theme): extract `load_theme()`

--- .local/bin/switch_nvim_theme.py
+++ .local/bin/switch_nvim_theme.py
@@ -29,28 +29,26 @@ def get_nvim_socket_list() -> Iterator[str]:
         raise SystemExit(0)
 
 
+def load_theme(color):
+    for path in get_nvim_socket_list():
+        try:
+            with attach("socket", path=path) as nvim:
+                nvim.vars["mycolor"] = color
+                args = [expanduser("~/.config/nvim/plugin/colors.lua")]
+                nvim.lua.vim.cmd({"cmd": "source", "args": args})
+        # neovim may exit unexpectedly
+        # but to tell if `OSError` is caused by that is hard
+        except OSError as e:
+            code = e.errno
+            if code in {22, None}:
+                remove(path)
+            else:
+                raise
+
+
 def main():
-    def load_theme(color):
-        for path in get_nvim_socket_list():
-            try:
-                with attach("socket", path=path) as nvim:
-                    nvim.vars["mycolor"] = color
-                    nvim.lua.vim.cmd({"cmd": cmd, "args": args})
-            # neovim may exit unexpectedly
-            # but to tell if `OSError` is caused by that is hard
-            except OSError as e:
-                code = e.errno
-                if code in {22, None}:
-                    remove(path)
-                else:
-                    raise
-
-    cmd = "source"
-    colors_file = expanduser("~/.config/nvim/plugin/colors.lua")
-    args = [colors_file]  # h: vim.cmd()
-
     if len(argv) < 2:
-        load_theme(False)  # to clear mycolor variable
+        load_theme(False)  # to reset `mycolor` variable
     else:
         color = argv[1]
         if color in {"light", "dark"}:
