gaesa/dotfiles

commit 0d40f063fe9335b90cfef64f3abd1fda7baeab1f

refactor(mime): replace `str` with `tuple[str, str]`

.config/lf/opener.py
@@ -8,7 +8,7 @@ from os import environ, getenv
 from configparser import ConfigParser, SectionProxy
 
 
-def get_mime_type(file: str) -> str:
+def get_mime_type(file: str) -> tuple[str, str]:
     # `xdg-mime query filetype` are better than
     # `file -Lb --mime_type` & `mimetypes.guess_type()`
     # although both of them are not perfect
@@ -16,21 +16,17 @@ def get_mime_type(file: str) -> str:
     # `.md` (with CJK character), `.ts`,
     # `.m4a`, `.tm`, `.xopp`, `.org`, `.scm`
     extension = splitext(file)[1]
-    return (
-        run(
-            ["file", "-Lb", "--mime-type", file],
-            check=True,
-            capture_output=True,
-            text=True,
-        ).stdout.rstrip()
+    args = (
+        ["file", "-Lb", "--mime-type", file]
         if extension in {".ts", ".bak"}
-        else run(
-            ["xdg-mime", "query", "filetype", file],
-            check=True,
-            capture_output=True,
-            text=True,
-        ).stdout.rstrip()
+        else ["xdg-mime", "query", "filetype", file]
     )
+    string = run(args, capture_output=True, text=True, check=True).stdout.rstrip()
+    mime_type = tuple(string.split("/", 1))
+    if len(mime_type) != 2:
+        raise ValueError("Expected mime_type to split into exactly 2 strings")
+    else:
+        return mime_type
 
 
 def get_list_of_mimeapps(
@@ -279,7 +275,9 @@ def main():
     elif mime_type == "text/plain" and basename(file) == "playlist":
         run(["/usr/bin/mpv", f"--playlist={file}"])
     else:
-        default_desktops = get_default_desktops(mime_type, interactive=interactive)
+        default_desktops = get_default_desktops(
+            "/".join(mime_type), interactive=interactive
+        )
         open_with(default_desktops, file) if interactive else open(
             default_desktops, file  # pyright: ignore [reportGeneralTypeIssues]
         )

.config/lf/preview.py
@@ -73,47 +73,52 @@ class Switch:
 def create_switch_case(file):
     def create_archive_case():
         switch[
-            "application/x-compressed-tar",
-            "application/x-tar",
-            "application/x-archive",
-            "application/x-bzip",
-            "application/x-bzip-compressed-tar",
-            "application/vnd.ms-cab-compressed",
-            "application/gzip",
-            "application/x-java-archive",
-            "application/x-lzma",
-            "application/x-lz4",
-            "application/x-xz-compressed-tar",
-            "application/x-xz",
-            "application/x-xpinstall",
-            "application/x-compress",
-            "application/zip",
+            ("application", "x-compressed-tar"),
+            ("application", "x-tar"),
+            ("application", "x-archive"),
+            ("application", "x-bzip"),
+            ("application", "x-bzip-compressed-tar"),
+            ("application", "vnd.ms-cab-compressed"),
+            ("application", "gzip"),
+            ("application", "x-java-archive"),
+            ("application", "x-lzma"),
+            ("application", "x-lz4"),
+            ("application", "x-xz-compressed-tar"),
+            ("application", "x-xz"),
+            ("application", "x-xpinstall"),
+            ("application", "x-compress"),
+            ("application", "zip"),
         ] = lambda: run(["atool", "--list", "--", file], check=True)
-        switch["application/vnd.rar"] = lambda: run(
+        switch[("application", "vnd.rar")] = lambda: run(
             ["unrar", "lt", "-p-", "--", file], check=True
         )
-        switch["application/x-7z-compressed"] = lambda: run(
+        switch[("application", "x-7z-compressed")] = lambda: run(
             ["7z", "l", "-p", "--", file], check=True
         )
 
     def create_document_case():
         switch[
-            "application/vnd.oasis.opendocument.text",
-            "application/vnd.oasis.opendocument.spreadsheet",
+            ("application", "vnd.oasis.opendocument.text"),
+            ("application", "vnd.oasis.opendocument.spreadsheet"),
         ] = lambda: run(["odt2txt", file], check=True)
         switch[
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            (
+                "application",
+                "vnd.openxmlformats-officedocument.wordprocessingml.document",
+            ),
         ] = lambda: run(["pandoc", "-s", "-t", "gfm", "--", file], check=True)
 
     def create_other_case():
-        switch["application/x-bittorrent"] = lambda: run(
+        switch[("application", "x-bittorrent")] = lambda: run(
             ["transmission-show", "--", file], check=True
         )
-        switch["text/html", "application/xhtml+xml"] = lambda: run(
+        switch[("text", "html"), ("application", "xhtml+xml")] = lambda: run(
             ["w3m", "-dump", file], check=True
         )
         switch[
-            "application/xml", "application/json", "application/x-shellscript"
+            ("application", "xml"),
+            ("application", "json"),
+            ("application", "x-shellscript"),
         ] = lambda: preview_text(file)
 
     def default(mime_type_main: str):
@@ -153,9 +158,9 @@ def fallback_to_file_cmd(file):
     print(res, end="")
 
 
-def fallback_to_non_image(file, mime_type, mime_type_main):
+def fallback_to_non_image(file: str, mime_type: tuple[str, str]):
     switch = create_switch_case(file)
-    switch(mime_type, mime_type_main)
+    switch(mime_type, mime_type[0])
 
 
 def print_pure_image(file: str):
@@ -163,8 +168,8 @@ def print_pure_image(file: str):
     raise SystemExit(1)
 
 
-def print_cache_image(file: str):
-    print_image(thumb.main(file=file))
+def print_cache_image(file: str, mime_type: tuple[str, str]):
+    print_image(thumb.main(file=file, mime_type=mime_type))
     raise SystemExit(1)
 
 
@@ -174,23 +179,21 @@ def get_mime_type_main(mime_type):
     return mime_type_main
 
 
-def fallback(file, mime_type, mime_type_main):
-    from os import environ
-
-    environ["mime_type"] = mime_type
+def fallback(file: str, mime_type: tuple[str, str]):
+    mime_type_main = mime_type[0]
     if mime_type_main == "video":
-        print_cache_image(file)
+        print_cache_image(file, mime_type)
     elif mime_type_main == "audio":
         if audio_has_cover(file):
-            print_cache_image(file)
+            print_cache_image(file, mime_type)
         else:
             run(["mediainfo", "--", file], check=True)
     elif mime_type == "application/pdf":
-        print_cache_image(file)
+        print_cache_image(file, mime_type)
     elif mime_type == "application/epub+zip":
-        print_cache_image(file)
+        print_cache_image(file, mime_type)
     else:
-        fallback_to_non_image(file, mime_type, mime_type_main)
+        fallback_to_non_image(file, mime_type)
 
 
 def main():
@@ -200,12 +203,10 @@ def main():
         fallback_to_file_cmd(file)
     else:
         mime_type = get_mime_type(file)
-        mime_type_main = get_mime_type_main(mime_type)
-
-        if mime_type_main == "image":
+        if mime_type[0] == "image":
             print_pure_image(file)
         else:
-            fallback(file, mime_type, mime_type_main)
+            fallback(file, mime_type)
 
 
 if __name__ == "__main__":

.config/lf/thumb.py
@@ -7,12 +7,12 @@ from os.path import (
     expanduser,
     join,
 )
-from os import makedirs, environ
+from os import makedirs
 from sys import argv
 from my_os import json_read, json_write
 
 
-def gen_thumb(media, thumb_path):
+def gen_thumb(media: str, thumb_path: str, mime_type: tuple[str, str] | None = None):
     from subprocess import DEVNULL, run
 
     def gen_for_video():
@@ -78,14 +78,23 @@ def gen_thumb(media, thumb_path):
         )
         raise SystemExit(mes)
 
-    mime_type = environ["mime_type"]
-    if mime_type.startswith("video"):
+    def get_mime_type() -> tuple[str, str]:
+        if mime_type is None:
+            from opener import get_mime_type as get_type
+
+            return get_type(media)
+        else:
+            return mime_type
+
+    mime_type = get_mime_type()
+    main = mime_type[0]
+    if main == "video":
         gen_for_video()
-    elif mime_type.startswith("audio"):
+    elif main == "audio":
         gen_for_audio()
-    elif mime_type == "application/pdf":
+    elif mime_type == ("application", "pdf"):
         gen_for_pdf()
-    elif mime_type == "application/epub+zip":
+    elif mime_type == ("application", "epub+zip"):
         gen_for_epub()
     else:
         exit_with_msg()
@@ -132,14 +141,14 @@ def upd_index_and_get_thumb(index, media):
     return thumb
 
 
-def upd_thumb(media: str, thumb_path: str):
+def upd_thumb(media: str, thumb_path: str, mime_type: tuple[str, str] | None = None):
     if isfile(thumb_path):
         return
     else:
-        gen_thumb(media, thumb_path)
+        gen_thumb(media, thumb_path, mime_type)
 
 
-def main(file: str | None = None):
+def main(file: str | None = None, mime_type: tuple[str, str] | None = None):
     file = argv[1] if file is None else file
 
     index_root = expanduser("~/.cache/lf_thumb")
@@ -149,7 +158,7 @@ def main(file: str | None = None):
 
     thumb = upd_index_and_get_thumb(index, file)
     thumb_path = join(cache_root, thumb)
-    upd_thumb(file, thumb_path)
+    upd_thumb(file, thumb_path, mime_type)
 
     return thumb_path
 

.local/bin/mime
@@ -2,4 +2,4 @@
 from opener import get_mime_type
 from sys import argv
 
-print(get_mime_type(" ".join(argv[1:])))
+print("/".join(get_mime_type(" ".join(argv[1:]))))

commit cd574cefc3a3bac399ef0642f4b973f5cfbb77ec

fix(opener): update type of `mime_type`
This should fix a bug introduced by `0d40f063`.

.config/lf/opener.py
@@ -280,9 +280,9 @@ def main():
         file = argv[1]
 
     mime_type = get_mime_type(file)
-    if mime_type == "application/x-executable":
+    if mime_type == ("application", "x-executable"):
         run([file], check=True)
-    elif mime_type == "text/plain" and basename(file) == "playlist":
+    elif mime_type == ("text", "plain") and basename(file) == "playlist":
         run(["/usr/bin/mpv", f"--playlist={file}"])
     else:
         default_desktops = get_default_desktops(