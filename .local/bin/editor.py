f42231322f71fc7fce7a2505150386689fe017e6
gaesa <71256557+gaesa@users.noreply.github.com>
2023-10-31 13:22:19 +0800

    fix(editor): also check empty tuple

--- .local/bin/editor.py
+++ .local/bin/editor.py
@@ -5,7 +5,7 @@ from subprocess import run
 
 def edit(files: list[str] | tuple[str, ...] | None = None):
     files = argv[1:] if files is None else files
-    if files == []:
+    if files == () or files == []:
         run(["/usr/bin/nvim"])
     else:
         run(["/usr/bin/nvim", "-o", *files, "-c", "wincmd H"])
