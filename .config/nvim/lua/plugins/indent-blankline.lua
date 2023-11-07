0b363751cf26fa1a6c253569916e86c3e7682fd3
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-07 13:39:40 +0800

    fix(nvim): also hide first tab indent level

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -7,6 +7,7 @@ return {
         config = function(_, opts)
             local hooks = require("ibl.hooks") -- must be set before setup() to initialize hook functions
             hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+            hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
             require("ibl").setup(opts)
 
             -- https://github.com/lukas-reineke/indent-blankline.nvim/pull/685
