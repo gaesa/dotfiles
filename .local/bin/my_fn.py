38c0a2ab1f85b9b3d39bc84994cefe5f44a3ea65
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-09 13:49:01 +0800

    chore(my_fn): remove an unnecessary function

--- .local/bin/my_fn.py
+++ .local/bin/my_fn.py
@@ -1,4 +1,3 @@
-from inspect import isfunction, getmodule, stack
 from typing import Callable
 
 
@@ -31,21 +30,3 @@ def debug_fn(old_fn: Callable, name: str = ""):
         return value
 
     return new_fn
-
-
-def is_global_user_fn(name: str) -> bool:
-    caller_frame = stack()[1][0]
-    caller_globals = caller_frame.f_globals
-    if name in caller_globals:
-        caller_module = getmodule(caller_frame)
-        if caller_module is not None:
-            fn = eval(name, caller_globals, None)
-            fn_module = getmodule(fn)
-            if fn_module is not None:
-                return isfunction(fn) and (fn_module == caller_module)
-            else:
-                return False
-        else:
-            return False
-    else:
-        return False
