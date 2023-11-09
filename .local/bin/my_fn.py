17be3070cb66ad09416cca2ab0e5a8a14d9f0cfc
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-09 11:21:32 +0800

    chore(my_fn): change names

--- .local/bin/my_fn.py
+++ .local/bin/my_fn.py
@@ -2,11 +2,11 @@ from inspect import isfunction, getmodule, stack
 from typing import Callable
 
 
-def add_post_hook(post_hook: Callable):
+def after(post_fn: Callable):
     def decorator(old_fn: Callable) -> Callable:
         def new_fn(*args, **kwargs):
             value = old_fn(*args, **kwargs)
-            post_hook()
+            post_fn()
             return value
 
         return new_fn
