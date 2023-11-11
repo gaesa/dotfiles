b786451547ae0319e0e747d36111304f4f710e27
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-11 18:00:08 +0800

    fix(lf): attempt to fix `"a=d,d=A" after closing lf`
    https://github.com/gokcehan/lf/issues/1189

--- .config/lf/preview_clean.py
+++ .config/lf/preview_clean.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
-from subprocess import run
+from base64 import standard_b64encode
+
+
+def serialize(payload: bytes | str = b"", **cmd: dict[str, str]) -> bytes:
+    "Modified from https://github.com/kovidgoyal/kitty/blob/master/kittens/tui/images.py#L382-L397."
+
+    cmds = ",".join(map(lambda kv: f"{kv[0]}={kv[1]}", cmd.items()))
+    ans = [b"\033_G", cmds.encode("ascii")]
+    if payload not in {"", b""}:
+        ans.extend(
+            (
+                b";",
+                (
+                    standard_b64encode(payload.encode())
+                    if isinstance(payload, str)
+                    else payload
+                ),
+                b"\033\\",
+            )
+        )
+    else:
+        ans.append(b"\033\\")
+    return b"".join(ans)
 
 
 def main():
+    # `sys.stdout` doesn't work as expected here, we have to access the low-level device `/dev/tty` directly.
     with open("/dev/tty", "w") as tty:
-        run(
-            [
-                "kitten",
-                "icat",
-                "--stdin",
-                "no",
-                "--transfer-mode",
-                "memory",
-                "--clear",
-            ],
-            check=True,
-            stdout=tty,
+        tty.buffer.write(
+            serialize(a="d", d="A")  # pyright: ignore [reportGeneralTypeIssues]
         )
 
 
