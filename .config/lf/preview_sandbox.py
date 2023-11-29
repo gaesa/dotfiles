3c01a384a2a6207ee8c6917742be006a0cbba219
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-30 04:49:46 +0800

    refactor(preview): use `pathlib.Path` & `dirs.Xdg`

--- .config/lf/preview_sandbox.py
+++ .config/lf/preview_sandbox.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python3
 from subprocess import run
-from os import getenv, makedirs
-from os.path import expanduser, join, isdir, realpath
+from pathlib import Path
 from sys import argv
 from typing import Iterable, Literal
-from my_utils.seq import fallback, flatmap
+from tempfile import gettempdir
+from my_utils.seq import flatmap
+from my_utils.dirs import Xdg
 
 
-def get_cmd(CURRENT_FILE: str, XDG: tuple[str, str, str]) -> list[str]:
+def get_cmd(CURRENT_FILE: str, XDG: tuple[Path, Path, Path]) -> list[str | Path]:
     opt_table: dict[Literal["ro", "dev"] | None, str] = {
         "ro": "--ro-bind",
         "dev": "--dev-bind",
         None: "--bind",
     }
 
-    def bind(opt_abbr: Literal["ro", "dev"] | None, dirs: list[str]) -> Iterable[str]:
+    def bind(
+        opt_abbr: Literal["ro", "dev"] | None, dirs: list[str | Path]
+    ) -> Iterable[str | Path]:
         opt = [opt_table[opt_abbr]]
         return flatmap(lambda dir: opt + [dir] * 2, dirs)
 
-    def bind_thumb() -> Iterable[str]:
-        path = join(XDG_CACHE_HOME, "lf_thumb")
-        None if isdir(path) else makedirs(path)
+    def bind_thumb() -> Iterable[str | Path]:
+        path = Path(XDG_CACHE_HOME, "lf_thumb")
+        None if path.is_dir() else path.mkdir(parents=True, exist_ok=True)
         return bind(None, [path])
 
-    def bind_pycache() -> Iterable[str]:
-        prefix = join(XDG_CACHE_HOME, "python")
-        dir_lf = join(prefix, join(XDG_CONFIG_HOME, "lf")[1:])
-        dir_utils = join(prefix, join(XDG_DATA_HOME, "python/lib/my_utils")[1:])
-        dir_usr = join(prefix, "usr")
+    def bind_pycache() -> Iterable[str | Path]:
+        prefix = Path(XDG_CACHE_HOME, "python")
+        dir_lf = Path(prefix, *Path(XDG_CONFIG_HOME, "lf").parts[1:])
+        dir_utils = Path(prefix, *Path(XDG_DATA_HOME, "python/lib/my_utils").parts[1:])
+        dir_usr = Path(prefix, "usr")
         return bind(None, [dir_lf, dir_utils, dir_usr])
 
     XDG_CACHE_HOME, XDG_CONFIG_HOME, XDG_DATA_HOME = XDG
-    preview = join(XDG_CONFIG_HOME, "lf/preview.py")
+    preview = Path(XDG_CONFIG_HOME, "lf/preview.py")
     return [
         "/usr/bin/bwrap",
         *bind("ro", ["/usr/bin", "/usr/share", "/usr/lib", "/usr/lib64"]),
@@ -42,10 +45,10 @@ def get_cmd(CURRENT_FILE: str, XDG: tuple[str, str, str]) -> list[str]:
             "ro",
             [
                 preview,
-                join(XDG_CONFIG_HOME, "lf/opener.py"),
-                join(XDG_CONFIG_HOME, "lf/thumb.py"),
-                join(XDG_CONFIG_HOME, "bat/config"),
-                join(XDG_DATA_HOME, "python/lib/my_utils/os.py"),
+                Path(XDG_CONFIG_HOME, "lf/opener.py"),
+                Path(XDG_CONFIG_HOME, "lf/thumb.py"),
+                Path(XDG_CONFIG_HOME, "bat/config"),
+                Path(XDG_DATA_HOME, "python/lib/my_utils/os.py"),
                 CURRENT_FILE,
             ],
         ),
@@ -60,20 +63,13 @@ def get_cmd(CURRENT_FILE: str, XDG: tuple[str, str, str]) -> list[str]:
 
 
 def main():
-    HOME = expanduser("~")
-    XDG_DATA_HOME: str = fallback(
-        lambda: getenv("XDG_DATA_HOME"), lambda: join(HOME, ".local/share")
-    )
-    XDG_CACHE_HOME: str = fallback(
-        lambda: getenv("XDG_CACHE_HOME"), lambda: join(HOME, ".cache")
-    )
-    XDG_CONFIG_HOME: str = fallback(
-        lambda: getenv("XDG_CONFIG_HOME"), lambda: join(HOME, ".config")
-    )
-    TMPDIR = getenv("TMPDIR", "/tmp")
+    XDG_DATA_HOME = Xdg.user_data_path()
+    XDG_CACHE_HOME = Xdg.user_cache_path()
+    XDG_CONFIG_HOME = Xdg.user_config_path()
+    TMPDIR = gettempdir()
 
-    with open(join(TMPDIR, "lf.log"), "w") as log_file:
-        argv[1] = realpath(argv[1], strict=True)
+    with open(Path(TMPDIR, "lf.log"), "w") as log_file:
+        argv[1] = str(Path(argv[1]).resolve(strict=True))
         process = run(
             get_cmd(argv[1], (XDG_CACHE_HOME, XDG_CONFIG_HOME, XDG_DATA_HOME)),
             stderr=log_file,
