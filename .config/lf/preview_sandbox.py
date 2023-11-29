5bbd9f7e6d65cd86183b23f6373e948e9fa6afea
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-30 06:48:06 +0800

    refactor(preview): prefer `tuple` to `list`

--- .config/lf/preview_sandbox.py
+++ .config/lf/preview_sandbox.py
@@ -16,43 +16,43 @@ def get_cmd(CURRENT_FILE: str, XDG: tuple[Path, Path, Path]) -> list[str | Path]
     }
 
     def bind(
-        opt_abbr: Literal["ro", "dev"] | None, dirs: list[str | Path]
+        opt_abbr: Literal["ro", "dev"] | None, dirs: tuple[str | Path, ...]
     ) -> Iterable[str | Path]:
-        opt = [opt_table[opt_abbr]]
-        return flatmap(lambda dir: opt + [dir] * 2, dirs)
+        opt = opt_table[opt_abbr]
+        return flatmap(lambda dir: (opt, dir, dir), dirs)
 
     def bind_thumb() -> Iterable[str | Path]:
         path = Path(XDG_CACHE_HOME, "lf_thumb")
         None if path.is_dir() else path.mkdir(parents=True, exist_ok=True)
-        return bind(None, [path])
+        return bind(None, (path,))
 
     def bind_pycache() -> Iterable[str | Path]:
         prefix = Path(XDG_CACHE_HOME, "python")
         dir_lf = Path(prefix, *Path(XDG_CONFIG_HOME, "lf").parts[1:])
         dir_utils = Path(prefix, *Path(XDG_DATA_HOME, "python/lib/my_utils").parts[1:])
         dir_usr = Path(prefix, "usr")
-        return bind(None, [dir_lf, dir_utils, dir_usr])
+        return bind(None, (dir_lf, dir_utils, dir_usr))
 
     XDG_CACHE_HOME, XDG_CONFIG_HOME, XDG_DATA_HOME = XDG
     preview = Path(XDG_CONFIG_HOME, "lf/preview.py")
     return [
         "/usr/bin/bwrap",
-        *bind("ro", ["/usr/bin", "/usr/share", "/usr/lib", "/usr/lib64"]),
+        *bind("ro", ("/usr/bin", "/usr/share", "/usr/lib", "/usr/lib64")),
         *("--symlink", "/usr/bin", "/bin"),
         *("--symlink", "/usr/lib64", "/lib64"),
         *("--proc", "/proc"),
         *bind(
             "ro",
-            [
+            (
                 preview,
                 Path(XDG_CONFIG_HOME, "lf/opener.py"),
                 Path(XDG_CONFIG_HOME, "lf/thumb.py"),
                 Path(XDG_CONFIG_HOME, "bat/config"),
                 Path(XDG_DATA_HOME, "python/lib/my_utils/os.py"),
                 CURRENT_FILE,
-            ],
+            ),
         ),
-        *bind("dev", ["/dev/tty", "/dev/null", "/dev/shm"]),
+        *bind("dev", ("/dev/tty", "/dev/null", "/dev/shm")),
         *bind_thumb(),
         *bind_pycache(),
         "--unshare-all",
