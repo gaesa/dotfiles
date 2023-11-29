37adde812761df642d0432a629145545e4d03c71
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-30 02:31:18 +0800

    refactor(py): move `get_mime_type` into `os.py`

--- .local/share/python/lib/my_utils/os.py
+++ .local/share/python/lib/my_utils/os.py
@@ -4,6 +4,48 @@ from typing import Callable
 from stat import S_IMODE
 
 
+def get_mime_type(file: str | Path) -> tuple[str, str]:
+    # `xdg-mime query filetype` are better than
+    # `file -Lb --mime_type` & `mimetypes.guess_type()`
+    # although both of them are not perfect
+    # problematic extensions:
+    # `.md` (with CJK character), `.ts`,
+    # `.m4a`, `.tm`, `.xopp`, `.org`, `.scm`
+    from subprocess import run
+
+    file = file if isinstance(file, Path) else Path(file)
+    extension = file.suffix
+
+    file_args = ["file", "-Lb", "--mime-type", "--", file]
+    args = (
+        file_args
+        if extension in {".ts", ".bak", ".txt", ".TXT"}
+        else [
+            "xdg-mime",
+            "query",
+            "filetype",
+            f"./{file}" if str(file).startswith("-") else file,
+        ]
+    )
+
+    string = run(args, capture_output=True, text=True, check=True).stdout.rstrip()
+    mime_type = tuple(string.split("/", 1))
+    if len(mime_type) != 2:
+        if args[0] == "xdg-mime":
+            new_str = run(
+                file_args, capture_output=True, text=True, check=True
+            ).stdout.rstrip()
+            new_type = tuple(new_str.split("/", 1))
+            if len(new_type) != 2:
+                raise ValueError(f"{file_args} returns: '{new_str}'")
+            else:
+                return new_type
+        else:
+            raise ValueError(f"{file_args} returns: '{string}'")
+    else:
+        return mime_type
+
+
 def json_read(file: str | Path):
     import json
 
