5534b5630f8554416c778951c5ae2f4bab227fc1
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-30 04:23:57 +0800

    feat(py): implement `get_mime_type_async`

---
+++ .local/share/python/lib/my_utils/aio.py
@@ -0,0 +1,22 @@
+import asyncio
+
+
+async def run(cmd: list[str], check: bool = False, text: bool = False):
+    p = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await p.communicate()
+    p.stdout, p.stderr = (  # pyright: ignore [reportGeneralTypeIssues]
+        (stdout.decode(), stderr.decode()) if text else (stdout, stderr)
+    )
+    if p.returncode == 0:
+        return p
+    else:
+        if check:
+            raise Exception(
+                f"Command {cmd} failed with exit code {p.returncode}, stderr: {stderr.decode()}"
+            )
+        else:
+            return p
