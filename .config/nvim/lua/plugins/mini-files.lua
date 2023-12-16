c380fab3663dfe3135dcb272187785d2d97f5f2b
gaesa <71256557+gaesa@users.noreply.github.com>
2023-12-16 14:47:29 +0800

    fix(nvim): make `mini-files` hijack `netrw`

--- .config/nvim/lua/plugins/mini-files.lua
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -3,7 +3,7 @@ return {
         "echasnovski/mini.files",
         cond = not vim.g.vscode,
         version = false,
-        event = "VeryLazy",
+        lazy = false,
         config = function()
             require("mini.files").setup()
             vim.keymap.set({ "n" }, "<leader>fm", "<cmd>lua MiniFiles.open()<cr>")
