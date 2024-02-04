e75d2f05238538044c66cc452d1197587773f635
gaesa <71256557+gaesa@users.noreply.github.com>
2024-02-04 23:43:46 +0800

    refactor(py): better way to check if sequences are empty

--- .local/bin/editor.py
+++ .local/bin/editor.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-from sys import argv
 from subprocess import run
+from sys import argv
+
+from my_utils.seq import is_empty
 
 
 def edit(files: list[str] | tuple[str, ...] | None = None):
     files = argv[1:] if files is None else files
-    if files == () or files == []:
+    if is_empty(files):
         run(["/usr/bin/nvim"])
     else:
         run(["/usr/bin/nvim", "-o", *files, "-c", "wincmd H"])
