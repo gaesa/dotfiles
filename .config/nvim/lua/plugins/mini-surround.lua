5d6342a9c5f74d603c33a511018d55ae212366c7
gaesa <71256557+gaesa@users.noreply.github.com>
2024-03-12 11:55:40 +0800

    fix(nvim): surround without adding spaces

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -3,6 +3,16 @@ return {
         "echasnovski/mini.surround",
         version = false,
         event = "VeryLazy",
-        config = true,
+        config = function()
+            require("mini.surround").setup({
+                -- See also: https://github.com/echasnovski/mini.nvim/issues/128
+                custom_surroundings = {
+                    ["("] = { input = { "%b()", "^.().*().$" }, output = { left = "(", right = ")" } },
+                    ["["] = { input = { "%b[]", "^.().*().$" }, output = { left = "[", right = "]" } },
+                    ["{"] = { input = { "%b{}", "^.().*().$" }, output = { left = "{", right = "}" } },
+                    ["<"] = { input = { "%b<>", "^.().*().$" }, output = { left = "<", right = ">" } },
+                },
+            })
+        end,
     },
 }
