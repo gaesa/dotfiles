46347a67618d45f2596fff5cac5410c2f13890bf
gaesa <71256557+gaesa@users.noreply.github.com>
2024-03-18 16:46:10 +0800

    chore(py): suppress diagnostic info

--- .local/share/python/lib/my_utils/aio.py
+++ .local/share/python/lib/my_utils/aio.py
@@ -8,7 +8,7 @@ async def run(cmd: list[str], check: bool = False, text: bool = False):
         stderr=asyncio.subprocess.PIPE,
     )
     stdout, stderr = await p.communicate()
-    p.stdout, p.stderr = (  # pyright: ignore [reportGeneralTypeIssues]
+    p.stdout, p.stderr = (  # pyright: ignore [reportAttributeAccessIssue]
         (stdout.decode(), stderr.decode()) if text else (stdout, stderr)
     )
     if p.returncode == 0:
