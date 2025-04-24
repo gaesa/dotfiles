2082893c60efe0409cc42101b03bc5771f13c945
gaesa <71256557+gaesa@users.noreply.github.com>
2025-04-24 12:38:45 +0000

    feat(plasma): add `env.sh`

---
+++ .config/plasma-workspace/env/env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    export PATH="$PATH:$HOME/.local/bin"
+fi
+
+# Rust
+if [[ ":$PATH:" != *":$XDG_DATA_HOME/cargo/bin:"* ]]; then
+    export PATH="$PATH:$XDG_DATA_HOME/cargo/bin"
+fi
+
+# Python
+export PYTHONPATH="$XDG_DATA_HOME/python/lib"
+export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
+export PYTHONUSERBASE="$XDG_DATA_HOME/python"
